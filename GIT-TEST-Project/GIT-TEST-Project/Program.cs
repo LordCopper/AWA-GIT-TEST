1513b38b6d3e70ebbaa737029b003015419fdeff
LordCopper <acoppero@gmail.com>
2015-09-24 11:37:47 +0200

    Kim får var med

--- GIT-TEST-Project/GIT-TEST-Project/Program.cs
+++ GIT-TEST-Project/GIT-TEST-Project/Program.cs
@@ -10,6 +10,10 @@ namespace GIT_TEST_Project
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the awsome Git Test preview!");
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+            Console.WriteLine("Good, now you can exit the application, good bye.");
             /* DETTA ÄR EN JÄTTE VIKTIG TEXT LORUM IPSUM LORUM IPSUM LORUM IPSUM LORUM IPSUM LORUM IPSUM
              LORUM IPSUM LORUM IPSUM LORUM IPSUM LORUM IPSUM LORUM IPSUM LORUM IPSUM LORUM IPSUM
               LORUM IPSUM LORUM IPSUM LORUM IPSUM LORUM IPSUM LORUM IPSUM LORUM IPSUM LORUM IPSUM
@@ -25,7 +29,7 @@ namespace GIT_TEST_Project
             Console.ReadLine();
             Car danielsBil = new Car();
         }
-    }
+        }
 
     class Car
     {
